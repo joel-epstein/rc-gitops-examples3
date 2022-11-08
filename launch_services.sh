c961c7cc1e67455397ffcb87abe9b0cdadf5bc3f
Joel Epstein <joel.epstein@greymatter.io>
2022-11-08 08:11:43 -0700

    add launch

--- launch_services.sh
+++ launch_services.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-# cue eval all_k8s.cue -e everything_yaml --out text -t number=50 -t namespace=foobar-1 | kubectl delete -f -
-cue eval all_k8s.cue -e everything_yaml --out text -t number=1 -t namespace=foobar-2 | kubectl apply -f -
+cue eval all_k8s.cue -e everything_yaml --out text -t number=50 -t namespace=foobar-1 | kubectl apply -f -
+# cue eval all_k8s.cue -e everything_yaml --out text -t number=1 -t namespace=foobar-2 | kubectl apply -f -
 # cue eval all_k8s.cue -e everything_yaml --out text -t number=50 -t namespace=foobar-3 | kubectl apply -f -
 # cue eval all_k8s.cue -e everything_yaml --out text -t number=50 -t namespace=foobar-4 | kubectl apply -f -
 # cue eval all_k8s.cue -e everything_yaml --out text -t number=50 -t namespace=foobar-5 | kubectl apply -f -
