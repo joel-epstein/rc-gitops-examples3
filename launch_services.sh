7155450b650496cc66607a8ff5689730313b116a
Joel Epstein <joel.epstein@greymatter.io>
2022-11-09 07:27:47 -0700

    increase to 150 peaches

--- launch_services.sh
+++ launch_services.sh
@@ -2,14 +2,14 @@
 
 set -ex
 
-cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-1 | kubectl apply -f -
-cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-2 | kubectl apply -f -
-cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-3 | kubectl apply -f -
-cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-4 | kubectl apply -f -
-cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-5 | kubectl apply -f -
-cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-6 | kubectl apply -f -
-cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-7 | kubectl apply -f -
-cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-8 | kubectl apply -f -
-cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-9 | kubectl apply -f -
-cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-10 | kubectl apply -f -
+cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-1 | kubectl delete -f -
+cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-2 | kubectl delete -f -
+cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-3 | kubectl delete -f -
+cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-4 | kubectl delete -f -
+cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-5 | kubectl delete -f -
+cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-6 | kubectl delete -f -
+cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-7 | kubectl delete -f -
+cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-8 | kubectl delete -f -
+cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-9 | kubectl delete -f -
+cue eval all_k8s.cue -e everything_yaml --out text -t number=100 -t namespace=foobar-10 | kubectl delete -f -
 
